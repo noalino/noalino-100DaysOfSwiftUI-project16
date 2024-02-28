fa73c269fc8ec8986fa263a7845efdfc0997bd86
Noalino <bgelineau.dev@gmail.com>
2024-02-28 10:30:22 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Noalino on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
